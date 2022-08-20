f15846ba21590b573fa0d7a21cadfaf2992c5a77
PilarczykM <marcinpilarczyk@me.com>
2022-08-20 17:45:28 +0200

    Create initial C# project. Add domain folder with entities

---
+++ Webinar/Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Category : AuditableEntity
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public ICollection<Post>? Posts { get; set; }
+}
---
+++ Webinar/Domain/Entities/Webinar.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Entities;
+
+public class Webinar
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public string? FacebookEventUrl { get; set; }
+
+    public string? SlidesUrl { get; set; }
+
+    public string? WatchFacebookLink { get; set; }
+
+    public string? WatchYoutubeLink { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public bool AlreadyHappend { get; set; }
+}
