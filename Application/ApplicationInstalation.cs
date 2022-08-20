7617c5c4601185f31a7e632657b13ee952a0598f
PilarczykM <marcinpilarczyk@me.com>
2022-08-20 19:36:07 +0200

    Create ApplicationInstalation.cs
    return meiatr and automapper servicers for application

---
+++ Application/ApplicationInstalation.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class ApplicationInstalation
+{
+    public static IServiceCollection addWebinarApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(assemblies: Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
