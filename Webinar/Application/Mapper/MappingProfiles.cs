a1a3c68808f66a482a26edc5cdee04307cf33cec
PilarczykM <marcinpilarczyk@me.com>
2022-08-20 19:17:49 +0200

    Create MappingProfiles.cs
    Add mapping profiler to map all usecases

---
+++ Webinar/Application/Mapper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using Application.Functions.Posts.Queries.GetPostDitail;
+using Application.Functions.Posts.Queries.GetPostsList;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapper;
+
+public class MappingProfiles : Profile
+{
+	public MappingProfiles()
+	{
+		CreateMap<Post, PostViewModel>().ReverseMap();
+	}
+}
