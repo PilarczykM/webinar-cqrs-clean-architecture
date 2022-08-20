503422e888fe8a2814e3c854e73f2966c1022094
PilarczykM <marcinpilarczyk@me.com>
2022-08-20 19:15:30 +0200

    Create application repositories

---
+++ Webinar/Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Contracts.Persistence;
+
+public interface IAsyncRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> AddAsync();
+    Task DeleteAsync();
+    Task<T> UpdateAsync();
+}
