a921e1aa125f1ef42c80cb2a2e3a6aabf916441f
PilarczykM <marcinpilarczyk@me.com>
2022-08-20 20:43:29 +0200

    Prepare query for getting list of categories

---
+++ Application/Functions/Categories/Queries/GetCategoriesList/CategoryView.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Functions.Categories.Queries.GetCategoriesList;
+
+public class CategoryView
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? DisplayName { get; set; }
+}
---
+++ Application/Functions/Categories/Queries/GetCategoriesList/GetCategoriesListQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Application.Functions.Categories.Queries.GetCategoriesList;
+
+public class GetCategoriesListQuery : IRequest<List<CategoryView>>
+{
+}
