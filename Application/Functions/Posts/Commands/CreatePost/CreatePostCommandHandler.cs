8d4d86812899a156f31a983ca503bbc714ec517c
PilarczykM <marcinpilarczyk@me.com>
2022-08-21 09:43:20 +0200

    CreatePost command
    Add create post command

---
+++ Application/Functions/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Contracts.Persistence;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Functions.Posts.Commands.CreatePost;
+
+public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, int>
+{
+    private readonly IMapper mapper;
+    private readonly IPostRepository postRepository;
+
+    public CreatePostCommandHandler(IMapper mapper, IPostRepository postRepository)
+    {
+        this.mapper = mapper;
+        this.postRepository = postRepository;
+    }
+
+    public async Task<int> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+    {
+
+        var post = this.mapper.Map<Post>(request);
+
+        post = await this.postRepository.AddAsync(post);
+
+        return post.PostId;
+    }
+}
