8a9153482fe93db878a60a8953e7c1100cf0507b
PilarczykM <marcinpilarczyk@me.com>
2022-08-21 11:05:02 +0200

    Create CreatePostCommandResponse.cs
    Create PostCommandResponde

---
+++ Application/Functions/Posts/Commands/CreatePost/CreatePostCommandResponse.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common;
+using FluentValidation.Results;
+
+namespace Application.Functions.Posts.Commands.CreatePost;
+
+public class CreatePostCommandResponse : BaseResponse
+{
+    public int? PostId { get; set; }
+
+    public CreatePostCommandResponse() : base() { }
+
+    public CreatePostCommandResponse(int? postId) : base()
+    {
+        this.PostId = postId;
+    }
+    public CreatePostCommandResponse(string message) : base(message) { }
+
+    public CreatePostCommandResponse(string message, bool success) : base(message, success) { }
+
+    public CreatePostCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+}
