002ba5a6b5b5194ca12147bda8e4faf3d62b65dc
PilarczykM <marcinpilarczyk@me.com>
2022-08-21 10:27:34 +0200

    Delete command
    Add delete command

---
+++ Application/Functions/Posts/Commands/DeletePost/DeletePostCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Functions.Posts.Commands.DeletePost;
+
+public class DeletePostCommand : IRequest
+{
+    public int PostId { get; set; }
+}
---
+++ Application/Functions/Posts/Commands/DeletePost/DeletePostCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Contracts.Persistence;
+using MediatR;
+
+namespace Application.Functions.Posts.Commands.DeletePost;
+
+public class DeletePostCommandHandler : IRequestHandler<DeletePostCommand>
+{
+    private readonly IPostRepository postRepository;
+
+    public DeletePostCommandHandler(IPostRepository postRepository)
+    {
+        this.postRepository = postRepository;
+    }
+
+    public async Task<Unit> Handle(DeletePostCommand request, CancellationToken cancellationToken)
+    {
+        var post = await this.postRepository.GetByIdAsync(request.PostId);
+        await this.postRepository.DeleteAsync(post);
+
+        return Unit.Value;
+    }
+}
