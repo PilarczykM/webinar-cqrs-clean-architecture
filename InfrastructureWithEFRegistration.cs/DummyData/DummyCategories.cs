54c83c29086f6cc85bf45b065444800cad69626d
PilarczykM <marcinpilarczyk@me.com>
2022-08-22 18:04:54 +0200

    Create new project infrastructure
    Add dummy data.

---
+++ InfrastructureWithEFRegistration.cs/DummyData/DummyCategories.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+
+namespace InfrastructureWithEFRegistration.cs.DummyData;
+
+public class DummyCategories
+{
+    public static List<Category> Get()
+    {
+        Category c1 = new()
+        {
+            CategoryId = DummySeed.Csharp,
+            Name = "CSharp",
+            DisplayName = "C#"
+        };
+
+        Category c2 = new()
+        {
+            CategoryId = DummySeed.Aspnet,
+            Name = "aspnet",
+            DisplayName = "ASP.NET"
+        };
+
+        Category c3 = new()
+        {
+            CategoryId = DummySeed.WindowsTricks,
+            Name = "triki-z-windows",
+            DisplayName = "Triki z Windows"
+        };
+
+        Category c4 = new()
+        {
+            CategoryId = DummySeed.Docker,
+            Name = "docker",
+            DisplayName = "Docker"
+        };
+
+        Category c5 = new()
+        {
+            CategoryId = DummySeed.Philosophy,
+            Name = "filozofia",
+            DisplayName = "Filozofia"
+        };
+
+
+        List<Category> p = new()
+        {
+            c1,
+            c3,
+            c2,
+            c4,
+            c5
+        };
+
+        return p;
+    }
+}
---
+++ InfrastructureWithEFRegistration.cs/DummyData/DummySeed.cs
@@ -0,0 +1,10 @@
+﻿namespace InfrastructureWithEFRegistration.cs.DummyData;
+
+public class DummySeed
+{
+    public static int Csharp = 1;
+    public static int Aspnet = 2;
+    public static int WindowsTricks = 3;
+    public static int Docker = 4;
+    public static int Philosophy = 5;
+}
