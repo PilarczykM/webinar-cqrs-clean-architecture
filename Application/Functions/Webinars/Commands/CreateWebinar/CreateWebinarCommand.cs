65034c2dccd0c7e2ba565147d76a35438b3866a2
PilarczykM <marcinpilarczyk@me.com>
2022-08-22 16:10:05 +0200

    Update CreateWebinarCommand.cs
    Add nullable

--- Application/Functions/Webinars/Commands/CreateWebinar/CreateWebinarCommand.cs
+++ Application/Functions/Webinars/Commands/CreateWebinar/CreateWebinarCommand.cs
@@ -6,19 +6,19 @@ public class CreateWebinarCommand : IRequest<CreateWebinarCommandResponse>
 {
     public int Id { get; set; }
 
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
 
-    public string FacebookEventUrl { get; set; }
+    public string? FacebookEventUrl { get; set; }
 
-    public string SlidesUrl { get; set; }
+    public string? SlidesUrl { get; set; }
 
-    public string WatchFacebookLink { get; set; }
+    public string? WatchFacebookLink { get; set; }
 
-    public string WatchYoutubeLink { get; set; }
+    public string? WatchYoutubeLink { get; set; }
 
     public DateTime Date { get; set; }
 
